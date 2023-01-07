35bfb52c6be3c33b2660119674bd6e1859a6cef8
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-01-07 19:00:35 +0000

    Login options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <h1>You apply for a job at McDonalds</h1>
 
 <div class="content">
-  <button>Login</button>
-  <button>Register</button>
+  <button>Login with Metamask</button>
+  <button>Login with WalletConnect</button>
+  <button>Login with Username + Password</button>
 </div>
